005d58a4ed0a79f9b7c0aca6ea78037d9cfb825f
mjoppich <gitlab@mjoppich.net>
2016-10-18 10:19:37 +0200

    add lua

--- build-in-place.sh
+++ build-in-place.sh
@@ -1,6 +1,6 @@
 apt-get update
 
-apt-get -y install build-essential git cmake libssl-dev
+apt-get -y install build-essential git cmake libssl-dev liblua5.3-dev
 apt-get -y install qtbase5-dev libqt5xmlpatterns5-dev
 
 mkdir build
--- src/main.cpp
+++ src/main.cpp
@@ -21,8 +21,38 @@
 #include <QPushButton>
 #include "bioGUIapp.h"
 
+extern "C" {
+#include <lua.h>                                /* Always include this when calling Lua */
+#include <lauxlib.h>                            /* Always include this when calling Lua */
+#include <lualib.h>                             /* Always include this when calling Lua */
+}
+
+void bail(lua_State *L, char *msg){
+    fprintf(stderr, "\nFATAL ERROR:\n  %s: %s\n\n",
+        msg, lua_tostring(L, -1));
+    exit(1);
+}
+
 int main(int argc, char **argv)
 {
+
+    lua_State *L;
+
+    L = luaL_newstate();                        /* Create Lua state variable */
+    luaL_openlibs(L);                           /* Load Lua libraries */
+
+    if (luaL_loadfile(L, "test.lua"))    /* Load but don't run the Lua script */
+    bail(L, "luaL_loadfile() failed");      /* Error out if file can't be read */
+
+    printf("In C, calling Lua\n");
+
+    if (lua_pcall(L, 0, 0, 0))                  /* Run the loaded Lua script */
+    bail(L, "lua_pcall() failed");          /* Error out if Lua file has an error */
+
+    printf("Back in C again\n");
+
+    lua_close(L);
+
 
     bioGUIapp* pApp = new bioGUIapp(argc, argv);
 

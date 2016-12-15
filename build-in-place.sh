829b71db17749ab033eb2cbe5c2675f7f2662fbf
mjoppich <gitlab@mjoppich.net>
2016-12-15 12:13:13 +0100

    wrong qt version ...

--- build-in-place.sh
+++ build-in-place.sh
@@ -1,7 +1,12 @@
 apt-get update
 
-apt-get -y install build-essential git cmake libssl-dev liblua5.3-dev
-apt-get -y install qtbase5-dev libqt5xmlpatterns5-dev
+apt-get -y install build-essential git cmake libssl-dev liblua5.3-dev libgl1-mesa-dev
+#apt-get -y install qtbase5-dev libqt5xmlpatterns5-dev
+
+wget http://download.qt.io/official_releases/qt/5.7/5.7.0/qt-opensource-linux-x64-5.7.0.run
+
+chmod u+x qt-opensource-linux-x64-5.7.0.run
+./qt-opensource-linux-x64-5.7.0.run -platform minimal
 
 mkdir build
 

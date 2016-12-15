ed667f7cc98459ca47e189f09ae14f03f34c77bb
Markus <gitlab@mjoppich.net>
2016-12-15 13:04:29 +0000

    Update build-in-place.sh

--- build-in-place.sh
+++ build-in-place.sh
@@ -1,6 +1,8 @@
 apt-get update
 
-apt-get -y install build-essential git cmake libssl-dev liblua5.3-dev libgl1-mesa-dev wget
+apt-get -y install build-essential git cmake libssl-dev liblua5.3-dev libgl1-mesa-dev wget libfontconfig1
+apt-get -y install libglu1-mesa-dev
+
 #apt-get -y install qtbase5-dev libqt5xmlpatterns5-dev
 
 wget http://download.qt.io/official_releases/qt/5.7/5.7.0/qt-opensource-linux-x64-5.7.0.run

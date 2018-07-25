440b750d4b78717e407bf34b67b8fd6f1271e2e4
Markus Joppich <github@mjoppich.net>
2018-07-25 17:21:14 +0200

    add new templates, fix bip

--- build-in-place.sh
+++ build-in-place.sh
@@ -5,12 +5,12 @@ apt-get -y install libglu1-mesa-dev
 
 #apt-get -y install qtbase5-dev libqt5xmlpatterns5-dev
 
-wget http://download.qt.io/official_releases/qt/5.7/5.7.0/qt-opensource-linux-x64-5.7.0.run
+wget http://download.qt.io/official_releases/qt/5.11/5.11.1/qt-opensource-linux-x64-5.11.1.run
 
-chmod u+x qt-opensource-linux-x64-5.7.0.run
-./qt-opensource-linux-x64-5.7.0.run --verbose -platform minimal --script ./silent_qt_install.qs
+chmod u+x qt-opensource-linux-x64-5.11.1.run
+./qt-opensource-linux-x64-5.11.1.run --verbose -platform minimal --script ./silent_qt_install.qs
 
-export PATH=/usr/local/qt/5.7/gcc_64/bin/:$PATH
+export PATH=/usr/local/qt/5.11.1/gcc_64/bin/:$PATH
 
 mkdir build
 
